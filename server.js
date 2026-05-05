aa1c904002e934e207a2f6740436905111314e1d
Gopi <tgopikrishna9pro@gmail.com>
2026-05-05 19:01:19 +0530

    fix port for railway

--- server.js
+++ server.js
@@ -1,33 +1,41 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const authMiddleware = require("./middleware/authMiddleware");
 
 const app = express();
 
-// 🔥 IMPORTANT LINE
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/taskmanager")
+// DB connection
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("DB connected"))
 .catch(err => console.log(err));
 
+// Routes
 const authRoutes = require("./routes/auth");
-app.use("/api/auth", authRoutes);
+const projectRoutes = require("./routes/project");
+const taskRoutes = require("./routes/task");
 
+app.use("/api/auth", authRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/tasks", taskRoutes);
+
+// Test routes
 app.get("/", (req, res) => {
   res.send("🔥 Backend is alive!");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
 app.get("/api/protected", authMiddleware, (req, res) => {
   res.json({
     message: "You accessed protected route 🎉",
     user: req.user
   });
 });
-const projectRoutes = require("./routes/project");
-app.use("/api/projects", projectRoutes);
-const taskRoutes = require("./routes/task");
-app.use("/api/tasks", taskRoutes);
+
+// 🔥 FIXED PORT
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
