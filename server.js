4c70e83e9b43dce69a18fc0e6c93963cb8962140
Gopi <tgopikrishna9pro@gmail.com>
2026-05-06 12:54:38 +0530

    added dotenv

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authMiddleware = require("./middleware/authMiddleware");
-
+require("dotenv").config();
 const app = express();
 
 app.use(express.json());
